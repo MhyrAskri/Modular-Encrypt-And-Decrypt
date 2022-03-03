e55d3aa655341c38e754da3addaae228041ccb0b
MhyrAskri <askari.m1379@gmail.com>
2022-03-03 09:50:36 +0330

    1st commit

---
+++ Code (arduino IDE)/code/code.ino
@@ -0,0 +1,110 @@
+/* Simple buffer example for low-memory conditions (Arduino) */
+
+#include "AESLib.h"
+#include <SoftwareSerial.h>
+
+#define BAUD 9600
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+AESLib aesLib;
+
+#define INPUT_BUFFER_LIMIT (128 + 1) // designed for Arduino UNO, not stress-tested anymore (this works with readBuffer[129])
+
+unsigned char cleartext[INPUT_BUFFER_LIMIT] = {0}; // THIS IS INPUT BUFFER (FOR TEXT)
+unsigned char ciphertext[2*INPUT_BUFFER_LIMIT] = {0}; // THIS IS OUTPUT BUFFER (FOR BASE64-ENCODED ENCRYPTED DATA)
+
+unsigned char readBuffer[18] = "username:password";
+
+// AES Encryption Key (same as in node-js example)
+byte aes_key[] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
+
+// General initialization vector (same as in node-js example) (you must use your own IV's in production for full security!!!)
+byte aes_iv[N_BLOCK] = { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA };
+
+// Generate IV (once)
+void aes_init() {
+  aesLib.gen_iv(aes_iv);
+  aesLib.set_paddingmode((paddingMode)0);
+}
+
+uint16_t encrypt_to_ciphertext(char * msg, uint16_t msgLen, byte iv[]) {
+  //Serial.println("Calling encrypt (string)...");
+  // aesLib.get_cipher64_length(msgLen);
+  int cipherlength = aesLib.encrypt((byte*)msg, msgLen, (char*)ciphertext, aes_key, sizeof(aes_key), iv);
+                   // uint16_t encrypt(byte input[], uint16_t input_length, char * output, byte key[],int bits, byte my_iv[]);
+  return cipherlength;
+}
+
+uint16_t decrypt_to_cleartext(byte msg[], uint16_t msgLen, byte iv[]) {
+  //Serial.print("Calling decrypt...; ");
+  uint16_t dec_bytes = aesLib.decrypt(msg, msgLen, (char*)cleartext, aes_key, sizeof(aes_key), iv);
+  //Serial.print("Decrypted bytes: "); Serial.println(dec_bytes);
+  return dec_bytes;
+}
+
+void setup() {
+  Serial.begin(BAUD);
+  mySerial.begin(BAUD);
+  Serial.setTimeout(60000);
+  mySerial.setTimeout(60000);
+  delay(2000);
+
+  aes_init(); // generate random IV, should be called only once? causes crash if repeated...
+
+}
+
+/* non-blocking wait function */
+void wait(unsigned long milliseconds) {
+  unsigned long timeout = millis() + milliseconds;
+  while (millis() < timeout) {
+    yield();
+  }
+}
+
+unsigned long loopcount = 0;
+
+// Working IV buffer: Will be updated after encryption to follow up on next block.
+// But we don't want/need that in this test, so we'll copy this over with enc_iv_to/enc_iv_from
+// in each loop to keep the test at IV iteration 1. We could go further, but we'll get back to that later when needed.
+
+// General initialization vector (same as in node-js example) (you must use your own IV's in production for full security!!!)
+byte enc_iv[N_BLOCK] =      { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA };
+byte enc_iv_to[N_BLOCK]   = { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA };
+byte enc_iv_from[N_BLOCK] = { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA };
+
+void loop() {
+
+  //Serial.print("readBuffer length: "); Serial.println(sizeof(readBuffer));
+  Serial.println((char*)readBuffer);
+
+   // must not exceed INPUT_BUFFER_LIMIT bytes; may contain a newline
+  sprintf((char*)cleartext, "%s", readBuffer);
+
+  // Encrypt
+  // iv_block gets written to, provide own fresh copy... so each iteration of encryption will be the same.
+  uint16_t msgLen = sizeof(readBuffer);
+  memcpy(enc_iv, enc_iv_to, sizeof(enc_iv_to));
+  uint16_t encLen = encrypt_to_ciphertext((char*)cleartext, msgLen, enc_iv);
+  //Serial.print("Encrypted length = "); Serial.println(encLen );
+  mySerial.println((char*)ciphertext);
+
+  //mySerial.println("Encrypted. Decrypting..."); mySerial.println(encLen );mySerial.flush();
+  
+  unsigned char base64decoded[50] = {0};
+  base64_decode((char*)base64decoded, (char*)ciphertext, encLen);
+  
+  memcpy(enc_iv, enc_iv_from, sizeof(enc_iv_from));
+  uint16_t decLen = decrypt_to_cleartext(base64decoded, strlen((char*)base64decoded), enc_iv);
+  //Serial.print("Decrypted cleartext of length: "); Serial.println(decLen);
+  Serial.print("Decrypted cleartext:\n"); Serial.println((char*)cleartext);
+
+  /*if (strcmp((char*)readBuffer, (char*)cleartext) == 0) {
+    Serial.println("Decrypted correctly.");
+  } else {
+    Serial.println("Decryption test failed.");
+  }*/
+  mySerial.println("-------------------------------");
+  Serial.println("-------------------------------");
+
+}
